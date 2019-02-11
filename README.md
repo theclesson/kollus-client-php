5714ea5ae28292cbf66fd2d98035d1fa929c0dad
razy <razy.dev@gmail.com>
2019-02-12 04:28:48 +0900

    a

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Kollus API Client
+
+
+add "clesson/kollus-client-php": "dev-master",
+
+remove "firebase/php-jwt": "^5.0"
---
+++ tests/KollusTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Clesson\Kollus\Kollus;
+use PHPUnit\Framework\TestCase;
+
+class KollusTest extends TestCase
+{
+    public function testGetToken()
+    {
+        $upload_key = 'asdb';
+        $kollus     = new Kollus();
+
+    }
+}
